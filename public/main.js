4e27895ac20c68eda8beef67dbb9b00d45b4f10e
Garrett-Lackey <garrettlackey2018@gmail.com>
2023-11-03 00:15:36 -0500

    reconnected buttons

--- public/main.js
+++ public/main.js
@@ -2,9 +2,9 @@ export async function create_Cat() {
     const mainDiv = document.querySelector(".pokedex-container");
 
     await create_CatCard(mainDiv);
-    // await create_CatVote(mainDiv);
+    await create_CatVote();
     // await create_CatComments(mainDiv);
-    // applyButtonFunctions();
+    applyButtonFunctions();
     // await setData(mainDiv.getAttribute("data-id"),
     //               mainDiv.getAttribute("data-url"),
     //               mainDiv.getAttribute("data-name"));
@@ -31,43 +31,29 @@ async function create_CatCard(div) {
     img.setAttribute("src", catData.url);
 }
 
-async function create_CatVote(div) {
-    const voteContainer = document.createElement("div");
-    const nextCatButt = document.createElement("button");
-    const upvoteCatButt = document.createElement("button");
-    const downvoteCatButt = document.createElement("button");
-    const voteText = document.createElement("span");
+async function create_CatVote() {
+    const upvoteCatButt = document.querySelector("#pokedex-upvote");
+    const downvoteCatButt = document.querySelector("#pokedex-downvote");
+    const voteText = document.querySelector("#pokedex-vote-screen");
 
-    voteContainer.setAttribute("class", "Cat_VoteBar");
-    nextCatButt.setAttribute("class", "Cat_Next");
-
-    upvoteCatButt.setAttribute("class", "Cat_Upvote");
     upvoteCatButt.setAttribute("data-checked", "unchecked");
-    
-    downvoteCatButt.setAttribute("class", "Cat_Downvote");
     downvoteCatButt.setAttribute("data-checked", "unchecked");
 
     voteText.innerText = "0";
-
-    const rightDiv = document.createElement("div");
-    rightDiv.style.display = "flex";
-    rightDiv.append(upvoteCatButt, voteText, downvoteCatButt);
-    voteContainer.append(nextCatButt, rightDiv)
-    div.append(voteContainer);
 }
 
 function applyButtonFunctions() {
     const clearVote = () => {
-        const upvote = document.querySelector(".Cat_Upvote")
-        const downvote = document.querySelector(".Cat_Downvote")
-        upvote.setAttribute("data-checked", "unchecked")
-        downvote.setAttribute("data-checked", "unchecked")
+        const upvote = document.querySelector("#pokedex-upvote");
+        const downvote = document.querySelector("#pokedex-downvote");
+        upvote.setAttribute("data-checked", "unchecked");
+        downvote.setAttribute("data-checked", "unchecked");
     }
 
     const nextCatFunc = async (e) => {
-        const name = document.querySelector("#Cat_Name");
-        const img = document.querySelector("#Cat_Img");
-        const mainDiv = document.querySelector(".Cat_Card");
+        const name = document.querySelector("#pokedex-details");
+        const img = document.querySelector("#pokedex-screen");
+        const mainDiv = document.querySelector(".pokedex-container");
 
         let catData = await fetch("https://api.thecatapi.com/v1/images/search?has_breeds=1&api_key=live_VYTaEUgfdnZx4OKDQrWiBjN8GGHBHXFXppa3GOx2NGMxuwn8aftNmVsZILeHeFZ2");
         catData = (await catData.json())[0];
@@ -77,7 +63,7 @@ function applyButtonFunctions() {
         img.setAttribute("src", catData.url);
         setData(catData.id);
     }
-    document.querySelector(".Cat_Next").addEventListener("click", nextCatFunc);
+    document.querySelector("#pokedex-next").addEventListener("click", nextCatFunc);
 
     const voteFunc = (e) => {
         clearVote();
@@ -85,8 +71,8 @@ function applyButtonFunctions() {
             e.currentTarget.setAttribute("data-checked", "checked")
         else e.currentTarget.setAttribute("data-checked", "unchecked")
     }
-    document.querySelector(".Cat_Upvote").addEventListener("click", voteFunc);
-    document.querySelector(".Cat_Downvote").addEventListener("click", voteFunc);
+    document.querySelector("#pokedex-upvote").addEventListener("click", voteFunc);
+    document.querySelector("#pokedex-downvote").addEventListener("click", voteFunc);
 }
 
 async function create_CatComments(div) {
